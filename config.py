82374eebe81ec3f0edb4a4e3d9a347ae8c09e9fa
Katharine Berry <katharine@kathar.in>
2009-11-20 21:54:46 +0000

    Creating initial repository structure

---
+++ config.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+config.py
+
+Created by Katharine Berry on 2009-11-21.
+Copyright (c) 2009 AjaxLife Developments. All rights reserved.
+"""
+
+API_KEY = 'Your API key' # From http://www.plurk.com/API
+PLURK_USER = 'TwitterbotUser'
+PLURK_PASSWORD = 'password'
+
+TWITTER_CONSUMER_KEY = 'your key' # From http://twitter.com/apps/new
+TWITTER_CONSUMER_SECRET = 'your secret'
+TWITTER_CALLBACK_URL = 'http://example.com/oauth.php' # Script that will call complete_oauth.py when requested
+
+MYSQL_SERVER = 'localhost'
+MYSQL_DATABASE = 'db'
+MYSQL_USER = 'user'
+MYSQL_PASSWORD = 'password'
