8552c7442685b10cdaeb1c91c6c1a40444b4820d
Katharine Berry <katharine@kathar.in>
2009-12-04 17:40:41 +0000

    Support for @mentions and DMs.

--- poll_plurk.py
+++ poll_plurk.py
@@ -34,12 +34,12 @@ for plurk in unread:
             if reply.user_id == plurk_api.user_id:
                 print "My own reply, aborting"
                 continue
-            c.execute("SELECT tweeter, tweet, in_reply_to, userid FROM plurkedtweets WHERE plurk = %s ORDER BY tweet ASC LIMIT 1", plurk.id)
+            c.execute("SELECT tweeter, tweet, in_reply_to, userid, is_dm FROM plurkedtweets WHERE plurk = %s ORDER BY tweet ASC LIMIT 1", plurk.id)
             tweetdata = c.fetchone()
             if tweetdata is None:
                 print "Couldn't get any data about the plurk"
                 continue
-            tweeter, tweet, in_reply_to, user_id = tweetdata
+            tweeter, tweet, in_reply_to, user_id, is_dm = tweetdata
             c.execute("SELECT twitter_key, twitter_secret, twitter_nick FROM users WHERE id = %s", user_id)
             twitterlogin = c.fetchone()
             if twitterlogin is None:
@@ -47,31 +47,36 @@ for plurk in unread:
                 continue
             twitter_key, twitter_secret, twitter_nick = twitterlogin
             oauth.set_access_token(twitter_key, twitter_secret)
-            tweet_text = reply.content_raw
-            reply_to_nick = re.match('@([0-9a-zA-Z_]+)', tweet_text)
-            reply_to_id = tweet
-            if reply_to_nick is None:
-                reply_to_nick = tweeter
-            else:
-                reply_to_nick = reply_to_nick.group(1)
-                tweet_text = tweet_text[len(reply_to_nick) + 2:]
-            
-            c.execute("SELECT tweeter, tweet FROM plurkedtweets WHERE plurk = %s AND tweeter = %s ORDER BY tweet DESC LIMIT 1", (plurk.id, reply_to_nick))
-            reply_to_data = c.fetchone()
-            if reply_to_data is None:
-                c.execute("SELECT tweeter, tweet FROM plurkedtweets WHERE plurk = %s AND tweeter = %s ORDER BY tweet DESC LIMIT 1", (plurk.id, tweeter))
-                reply_to_nick, reply_to_id = c.fetchone()
-            else:
-                reply_to_nick, reply_to_id = reply_to_data
-            
-            tweet_text = ("@%s %s" % (reply_to_nick, tweet_text))[:140]
-            
             twitter_api = tweepy.API(oauth)
-            status = twitter_api.update_status(status=tweet_text, in_reply_to_status_id=reply_to_id)
+            
+            tweet_text = reply.content_raw
+            if not is_dm:
+                reply_to_nick = re.match('@([0-9a-zA-Z_]+)', tweet_text)
+                reply_to_id = tweet
+                if reply_to_nick is None:
+                    reply_to_nick = tweeter
+                else:
+                    reply_to_nick = reply_to_nick.group(1)
+                    tweet_text = tweet_text[len(reply_to_nick) + 2:]
+            
+                c.execute("SELECT tweeter, tweet FROM plurkedtweets WHERE plurk = %s AND tweeter = %s ORDER BY tweet DESC LIMIT 1", (plurk.id, reply_to_nick))
+                reply_to_data = c.fetchone()
+                if reply_to_data is None:
+                    c.execute("SELECT tweeter, tweet FROM plurkedtweets WHERE plurk = %s AND tweeter = %s ORDER BY tweet DESC LIMIT 1", (plurk.id, tweeter))
+                    reply_to_nick, reply_to_id = c.fetchone()
+                else:
+                    reply_to_nick, reply_to_id = reply_to_data
+            
+                tweet_text = ("@%s %s" % (reply_to_nick, tweet_text))[:140]
+            
+                status = twitter_api.update_status(status=tweet_text, in_reply_to_status_id=reply_to_id)
+            else:
+                status = twitter_api.send_direct_message(user=tweeter, text=tweet_text[:140])
+                reply_to_id = None
             c.execute("""INSERT INTO
-                plurkedtweets (userid, tweeter, tweet, plurk, `date`, in_reply_to)
-                VALUES (%s, %s, %s, %s, NOW(), %s)""",
-                (user_id, twitter_nick, status.id, plurk.id, reply_to_id)
+                plurkedtweets (userid, tweeter, tweet, plurk, `date`, in_reply_to, is_dm)
+                VALUES (%s, %s, %s, %s, NOW(), %s, %s)""",
+                (user_id, twitter_nick, status.id, plurk.id, reply_to_id, int(is_dm))
             )
             print "Tweeted tweet %s \"%s\" in reply to %s for %s" % (status.id, tweet_text, reply_to_id, user_id)
     else:
--- poll_twitter.py
+++ poll_twitter.py
@@ -24,7 +24,8 @@ link = MySQLdb.connect(passwd=config.MYSQL_PASSWORD, user=config.MYSQL_USER, db=
 c = link.cursor()
 
 c.execute("""
-    SELECT id, plurk_uid, plurk_nick, twitter_nick, twitter_key, twitter_secret, last_tweet, last_mention, last_dm
+    SELECT id, plurk_uid, plurk_nick, twitter_nick, twitter_key, twitter_secret,
+           last_tweet, last_mention, last_dm, enable_home, enable_mention, enable_dm
     FROM users
     WHERE enabled = 1
     """
@@ -32,31 +33,82 @@ c.execute("""
 
 rows = c.fetchall()
 for row in rows:
-    user_id, plurk_uid, plurk_nick, twitter_nick, twitter_key, twitter_secret, last_tweet, last_mention, last_dm = row
+    user_id, plurk_uid, plurk_nick, twitter_nick, twitter_key, twitter_secret, \
+        last_tweet, last_mention, last_dm, enable_home, enable_mention, enable_dm = row
     oauth = tweepy.OAuthHandler(config.TWITTER_CONSUMER_KEY, config.TWITTER_CONSUMER_SECRET, config.TWITTER_CALLBACK_URL)
     oauth.set_access_token(twitter_key, twitter_secret)
     
     twitter_api = tweepy.API(oauth)
-    new_tweets = twitter_api.home_timeline(since_id=last_tweet, count=10) # Count could be 200, but then we'd spam 200 plurks.
+    all_tweets = []
+    if enable_home:
+        all_home = twitter_api.home_timeline(since_id=last_tweet, count=10)
+        all_tweets.extend(all_home) # Count could be 200, but then we'd spam 200 plurks.
+        if len(all_home) > 0:
+            c.execute("UPDATE users SET last_tweet = %s WHERE id = %s", (all_home[0].id, user_id))
+        print "Polled home - %s" % len(all_tweets)
+    if enable_mention:
+        all_mention = twitter_api.mentions(since_id=last_mention, count=10)
+        all_tweets.extend(all_mention)
+        if len(all_mention) > 0:
+            c.execute("UPDATE users SET last_mention = %s WHERE id = %s", (all_mention[0].id, user_id))
+        print "Polled mentions - %s" % len(all_tweets)
+    if enable_dm:
+        all_dm = twitter_api.direct_messages(since_id=last_dm, count=10)
+        all_tweets.extend(all_dm)
+        if len(all_dm) > 0:
+            c.execute("UPDATE users SET last_dm = %s WHERE id = %s", (all_dm[0].id, user_id))
+        print "Polled DMs - %s" % len(all_tweets)
+    new_tweets = []
+    
+    for tweet in all_tweets:
+        inserted = False
+        for i in xrange(0, len(new_tweets)):
+            if new_tweets[i].id == tweet.id:
+                inserted = True
+                break
+            if new_tweets[i].id > tweet.id:
+                new_tweets.insert(i, tweet)
+                inserted = True
+                break
+        if not inserted:
+            new_tweets.append(tweet)
+    
+    
     if len(new_tweets) == 0:
         continue
     
-    new_tweets.reverse()
+    #new_tweets.reverse()
     for tweet in new_tweets:
         c.execute("SELECT plurk FROM plurkedtweets WHERE tweet = %s", tweet.id)
         if c.fetchone() is not None:
             continue
-        if tweet.in_reply_to_status_id:
-            c.execute("SELECT plurk FROM plurkedtweets WHERE tweet = %s", tweet.in_reply_to_status_id)
+        is_dm = isinstance(tweet, tweepy.DirectMessage)
+        if is_dm:
+            print "Got a DM"
+            tweet.author = tweet.sender
+            c.execute("""SELECT plurk FROM plurkedtweets WHERE 
+                tweeter = %s AND is_dm = 1 AND userid = %s AND `date` > DATE_SUB(NOW(), INTERVAL 1 DAY)""",
+                (tweet.author.screen_name, user_id)
+            )
             in_reply_to_plurk = c.fetchone()
             if in_reply_to_plurk:
                 in_reply_to_plurk = in_reply_to_plurk[0]
-                print "Replying to plurk #%s" % in_reply_to_plurk
+            else:
+                in_reply_to_plurk = None
         else:
-            in_reply_to_plurk = None
-        plurk_text = '%s: %s' % (tweet.author.screen_name, re.sub(r'\B@([0-9a-zA-Z_]+)', r'twitter.com/\1 (@\1)', tweet.text))
+            if tweet.in_reply_to_status_id:
+                c.execute("SELECT plurk FROM plurkedtweets WHERE tweet = %s", tweet.in_reply_to_status_id)
+                in_reply_to_plurk = c.fetchone()
+                if in_reply_to_plurk:
+                    in_reply_to_plurk = in_reply_to_plurk[0]
+                    print "Replying to plurk #%s" % in_reply_to_plurk
+                else:
+                    in_reply_to_plurk = None
+            else:
+                in_reply_to_plurk = None
+        plurk_text = '%s%s: %s' % ('(DM) ' if is_dm else '', tweet.author.screen_name, re.sub(r'\B@([0-9a-zA-Z_]+)', r'twitter.com/\1 (@\1)', tweet.text))
         plurk_text = plurk_text.replace('http://','') # Save on characters.
-        parts = textwrap.wrap(PlurkAPI.Utilities.decode_html_entities(plurk_text.encode('utf-8')), 139)
+        parts = textwrap.wrap(PlurkAPI.Utilities.decode_html_entities(plurk_text).encode('utf-8'), 139)
         print parts
         try:
             if in_reply_to_plurk is None:
@@ -74,12 +126,10 @@ for row in rows:
                 plurk_api.Responses.add(plurk_id=reply_to, content=('…%s' % parts.pop(0)))
     
             c.execute("""
-                INSERT INTO plurkedtweets (userid, tweeter, tweet, plurk, `date`, in_reply_to)
-                VALUES (%s, %s, %s, %s, NOW(), %s)""",
-                (user_id, tweet.author.screen_name, tweet.id, reply_to, tweet.in_reply_to_status_id)
+                INSERT INTO plurkedtweets (userid, tweeter, tweet, plurk, `date`, in_reply_to, is_dm)
+                VALUES (%s, %s, %s, %s, NOW(), %s, %s)""",
+                (user_id, tweet.author.screen_name, tweet.id, reply_to, tweet.in_reply_to_status_id if not is_dm else None, int(is_dm))
             )
         except PlurkAPIError:
             print "Boo, error."
             pass
-    
-    c.execute("UPDATE users SET last_tweet = %s WHERE id = %s", (new_tweets[-1].id, user_id))
